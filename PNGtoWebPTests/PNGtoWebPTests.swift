ce0fd9db55c6e61e78aed3fcb24f2e2ad33da741
Kish Patel <kish@kishpatel.com>
2025-01-08 20:50:45 +0000

    Initial Commit

---
+++ PNGtoWebPTests/PNGtoWebPTests.swift
@@ -0,0 +1,17 @@
+//
+//  PNGtoWebPTests.swift
+//  PNGtoWebPTests
+//
+//  Created by Kish on 08/01/2025.
+//
+
+import Testing
+@testable import PNGtoWebP
+
+struct PNGtoWebPTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
